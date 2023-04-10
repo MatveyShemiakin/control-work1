f614ef30675d82a12cba3229f9593f429575cf14
Your Name <you@example.com>
2023-04-10 18:01:27 +0300

    Формирование нового массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Ввод исходного массива строк
+        Console.Write("Введите массив строк через пробел: ");
+        string[] arr = Console.ReadLine().Split();
+
+        // Формирование нового массива из строк, длина которых меньше или равна 3 символам
+        string[] new_arr = new string[arr.Length];
+        int j = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i].Length <= 3)
+            {
+                new_arr[j] = arr[i];
+                j++;
+            }
+        }
+       
+        
+        
+    }
+}
