80cfbbcdc93f3dc3b8582df32efb083fd6c3786d
Your Name <you@example.com>
2023-04-10 17:49:06 +0300

    Ввод исходного массива

---
+++ Program.cs
@@ -0,0 +1,11 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ввод исходного массива строк
+        Console.Write("Введите массив строк через пробел: ");
+        string[] arr = Console.ReadLine().Split();
+
+        
+    }
+}
